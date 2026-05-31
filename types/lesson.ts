a08c59db2b50b31c0e8cf8bc8871babda73fe0cf
Christiano <christianmapoe7@gmail.com>
2026-05-31 15:50:49 +0200

    Add abbreviation to PartOfSpeech type

--- types/lesson.ts
+++ types/lesson.ts
@@ -1,5 +1,5 @@
 export type Level = 'A1-A2' | 'B1-B2' | 'C1-C2';
-export type PartOfSpeech = 'noun' | 'verb' | 'adjective' | 'adverb' | 'phrasal verb';
+export type PartOfSpeech = 'noun' | 'verb' | 'adjective' | 'adverb' | 'phrasal verb' | 'abbreviation';
 export type SpeakerColor = 'purple' | 'green' | 'orange' | 'blue' | 'red';
 
 export interface VocabWord {
